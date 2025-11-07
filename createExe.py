a48a512762a7037aea7c25b928eb959751e853db
Berkantany <akinciberkant@gmail.com>
2025-11-07 08:36:03 +0300

    Add files via upload

---
+++ createExe.py
@@ -0,0 +1,4 @@
+## terminal ##
+## python -m PyInstaller --noconsole --name "YourApplicationName" your_script_name.py
+
+## Warning: You must know that PyInstaller does not automatically bundle the required Baumer neoapi files (.dll, .cti). After the build is complete, you must manually copy these files into the neoapi folder located inside your output directory (dist/YourApplicationName/).
